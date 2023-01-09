76fcbe998c28330f0c1b4e88c69bd8bef5852385
Tina Tran <tinatran@Tinas-iMac.local>
2023-01-09 16:06:42 -0600

    post for user fin

--- games/queries/games.py
+++ games/queries/games.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from queries.pool import pool
 
 class UserIn(BaseModel):
     username: str
     password: str
     email: str
+
+class UserOut(BaseModel):
+    id: int
+
+class UserRepository:
+    def create(self, user: UserIn) -> UserOut:
+        with pool.connection() as conn:
+            with conn.cursor() as postgres:
+                result = postgres.execute(
+                """
+                INSERT INTO Users
+                    (username, password, email)
+                VALUES
+                    (%s,%s,%s)
+                RETURNING id
+                """,
+                [
+                    user.username,
+                    user.password,
+                    user.email,
+                ]
+            )
+            id = result.fetchone()[0]
+            old_data = user.dict()
+            return UserOut(id=id, **old_data)
--- games/routers/games.py
+++ games/routers/games.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter
-from queries.games import UserIn
+from fastapi import APIRouter, Depends
+from queries.games import UserIn, UserRepository
 
 
 router = APIRouter()
 
 
 @router.post("/users")
-def create_user(Users: UserIn):
-    print ('users', Users)
-    return Users
+def create_user(
+    Users: UserIn,
+    repo:UserRepository = Depends()
+):
+    return repo.create(Users)
