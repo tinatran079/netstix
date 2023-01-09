bb83419c45b0d7bd73fcfc5ab43bf1995ba39ae9
Nick Gallo <nicolasfgallo1998@gmail.com>
2023-01-09 15:58:33 -0500

    tables created, migrations done

--- games/migrations/001_sample_migration.py
+++ games/migrations/001_sample_migration.py
@@ -2,40 +2,35 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE Games (
+        CREATE TABLE Users (
             id SERIAL PRIMARY KEY NOT NULL,
-            name VARCHAR(255) NOT NULL,
-            description TEXT NOT NULL,
-            background_image TEXT NOT NULL,
-            rating NUMERIC NOT NULL,
-            platforms TEXT ARRAY,
-            developer TEXT,
-            generes TEXT ARRAY
-
+            username VARCHAR(20) NOT NULL,
+            password VARCHAR(20) NOT NULL,
+            email VARCHAR(255) NOT NULL
 
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE Games;
+        DROP TABLE Users;
         """
     ],
+
+
     [
         # "Up" SQL statement
         """
-        CREATE TABLE big_dummy (
+        CREATE TABLE Reviews (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            subject VARCHAR(255) NOT NULL,
+            description TEXT NOT NULL,
+            user_id INTEGER NOT NULL REFERENCES Users("id") ON DELETE CASCADE
+
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE big_dummy;
+        DROP TABLE Reviews;
         """
     ]
 ]
