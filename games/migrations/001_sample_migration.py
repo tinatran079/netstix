57d44a00feee35ce07254381c9ccf1bd6e42debd
Nick Gallo <nicolasfgallo1998@gmail.com>
2023-01-09 13:37:33 -0500

    configured pg admin

--- games/migrations/001_sample_migration.py
+++ games/migrations/001_sample_migration.py
@@ -2,19 +2,22 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
+        CREATE TABLE Games (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            name VARCHAR(255) NOT NULL,
+            description TEXT NOT NULL,
+            background_image TEXT NOT NULL,
+            rating NUMERIC NOT NULL,
+            platforms TEXT ARRAY,
+            developer TEXT,
+            generes TEXT ARRAY
+
+
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
+        DROP TABLE Games;
         """
     ],
     [
---
+++ games/queries/games.py
@@ -0,0 +1,2 @@
+from fastapi import FastAPI
+from routers import games
