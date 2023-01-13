861e48c8af9efdbd49404c53ce16c3e75e256f51
Nick Gallo <nicolasfgallo1998@gmail.com>
2023-01-13 13:18:52 -0500

    api search done

--- games/routers/games.py
+++ games/routers/games.py
@@ -3,6 +3,9 @@ from queries.games import games_queries
 
 router = APIRouter()
 
-@router.get('/api/games')
+@router.get('/api/games/id')
 def get_games(repo: games_queries = Depends()):
     return repo.get_all_games()
+@router.get('/api/games')
+def get_games_by_search(repo: games_queries = Depends()):
+    return repo.get_games_by_search()
