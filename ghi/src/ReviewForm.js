440daf61986737d934ec4bc8427534955ef6ecea
Nick Gallo <nicolasfgallo1998@gmail.com>
2023-01-24 13:36:37 -0500

    front end auth request working

--- ghi/src/ReviewForm.js
+++ ghi/src/ReviewForm.js
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react';
+import { useToken } from './auth';
 
 const ReviewForm = () => {
+  const  [token]  = useToken()
+  // #console.log(token.access_token)
   const [formData, setFormData] = useState({
-    title: '',
-    body: '',
+    subject: '',
+    description: '',
+    account_id:5,
+    game_id:3796,
+    game_title:'test',
+
   })
 
   const id = 1
@@ -37,13 +44,14 @@ const ReviewForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    console.log(token)
     const ReviewUrl = 'http://localhost:8000/api/reviews';
     const fetchConfig = {
       method: "post",
       body: JSON.stringify(formData),
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
       },
     };
 
@@ -54,6 +62,9 @@ const ReviewForm = () => {
       setFormData({
         title: '',
         body: '',
+        account_id:'',
+        game_id:'',
+        game_title:'',
       });
     }
   }
@@ -65,14 +76,29 @@ return (
           <form onSubmit={handleSubmit} id="create-Review-form">
 
             <div className="form-floating mb-3">
-              <input onChange={handleFormChange} value={formData.title} placeholder="Review Title" required type="text" name="title" className="form-control" />
-              <label htmlFor="title">Title</label>
+              <input onChange={handleFormChange} value={formData.subject} placeholder="Review Title" required type="text" name="subject" className="form-control" />
+              <label htmlFor="subject">Title</label>
             </div>
 
             <div className="form-floating mb-3">
-              <input onChange={handleFormChange} value={formData.body} placeholder="Leave a review!" required type="text" name="body" className="form-control" />
-              <label htmlFor="body">Body</label>
+              <input onChange={handleFormChange} value={formData.description} placeholder="Leave a review!" required type="text" name="description" className="form-control" />
+              <label htmlFor="description">description</label>
             </div>
+              <div className="form-floating mb-3">
+              <input onChange={handleFormChange} value={formData.account_id} placeholder="Leave!" required type="number" name="Account Id" className="form-control" />
+              <label htmlFor="Account Id">Account Id</label>
+            </div>
+
+  <div className="form-floating mb-3">
+              <input onChange={handleFormChange} value={formData.game_id} placeholder="Lea a review!" required type="number" name="Game Id" className="form-control" />
+              <label htmlFor="game_id">Game Id</label>
+            </div>
+
+  <div className="form-floating mb-3">
+              <input onChange={handleFormChange} value={formData.game_title} placeholder="!" required type="text" name="Game Title" className="form-control" />
+              <label htmlFor="game_title">Game Title</label>
+            </div>
+
 
             <button className="btn btn-dark">Create</button>
           </form>
