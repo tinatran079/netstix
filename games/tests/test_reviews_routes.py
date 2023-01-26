d2036f0053e470045ddb200fdec23c7b9be5c80c
Tina Tran <tinatran@Tinas-iMac.local>
2023-01-26 12:15:02 -0600

    unit test done

--- games/tests/test_reviews_routes.py
+++ games/tests/test_reviews_routes.py
@@ -1,2 +1,23 @@
+from fastapi.testclient import TestClient
+from queries.reviews import ReviewQueries
+from main import app
+
+client = TestClient(app=app)
+
+class ReviewQueriesMock:
+    def get_reviews(self):
+        return []
+
 def test_get_reviews():
-    assert 1 == 1
+    # Arrange
+    app.dependency_overrides[ReviewQueries] = ReviewQueriesMock
+
+    # Act
+    res = client.get('/api/reviews')
+
+    # Assert
+    assert res.status_code == 200
+    assert res.json() == {'reviews': []}
+
+    # Cleanup
+    app.dependency_overrides = {}
