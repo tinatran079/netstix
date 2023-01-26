f2806f1ce47523fb2a510939b715821ad00bbf2c
Nick Gallo <nicolasfgallo1998@gmail.com>
2023-01-26 15:34:40 -0500

    finished writing Nicks test

--- games/tests/test_reviews_routes.py
+++ games/tests/test_reviews_routes.py
@@ -1,12 +1,18 @@
 from fastapi.testclient import TestClient
 from queries.reviews import ReviewQueries
+from routers.reviews import ReviewIn, ReviewOut
 from main import app
+import json
 
 client = TestClient(app=app)
 
 class ReviewQueriesMock:
     def get_reviews(self):
         return []
+
+    def create_review(self, review: ReviewIn) -> ReviewOut:
+        review_dict = review.dict()
+        return ReviewOut(id = 1, **review.dict())
 
 def test_get_reviews():
     # Arrange
@@ -20,4 +26,28 @@ def test_get_reviews():
     assert res.json() == {'reviews': []}
 
     # Cleanup
+    app.dependency_overrides = {}
+
+
+def test_create_reviews_protection():
+    # Arrange
+    app.dependency_overrides[ReviewQueries] = ReviewQueriesMock
+    review_body = {
+        "subject": "awesome",
+        "description": "boy! do I love this",
+        "account_id": 1,
+        "game_id": 300,
+        "game_title": "the best game ever",
+        "username": "gamer11",
+
+    }
+
+    # Act
+    res = client.post('/api/reviews', json.dumps(review_body))
+
+   # Assert
+    assert res.status_code == 401
+
+
+  # Cleanup
     app.dependency_overrides = {}
