007ac1a393232e8c1aaefffb62cf079deab3b65f
Griffin Scott <griffinoscott@gmail.com>
2023-01-27 14:58:34 -0800

    created last unit test

--- games/tests/test_reviews_routes.py
+++ games/tests/test_reviews_routes.py
@@ -1,11 +1,29 @@
 from fastapi.testclient import TestClient
 from queries.reviews import ReviewQueries
 from routers.reviews import ReviewIn, ReviewOut
-from routers.accounts import get_token
+from queries.accounts import AccountsQueries
+from routers.accounts import get_token, AccountIn, AccountOut
 from main import app
 import json
 
 client = TestClient(app=app)
+
+
+class AccountQueriesMock:
+    def create_account(self, account: AccountIn, hashed_password: str) -> AccountOut:
+        account_dict = account.dict()
+        return AccountOut(id=1, **account.dict())
+
+def test_create_account_format():
+    app.dependency_overrides[AccountsQueries] = AccountQueriesMock
+    account_body = {
+        "username": 123,
+        "hashed_password": "testpassword",
+        "email": "testusername"
+    }
+    res = client.post("/api/accounts", json.dumps(account_body))
+    assert res.status_code == 422
+    app.dependency_overrides = {}
 
 
 class ReviewQueriesMock:
